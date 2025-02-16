40790f58123507e9d83f7480020421bd6005e6cd
PabloBPafundi <128729717+PabloBPafundi@users.noreply.github.com>
2025-02-16 19:15:25 -0300

    Agregado de interfaz user

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
-
+import { IUserLogin } from '../../interfaces/IUserLogin'
 @Injectable({
   providedIn: 'root'
 })
@@ -15,11 +15,12 @@ export class ProductService {
   }
 
 
-  signInLocal(user: ): Observable<> {
+
+  signInLocal(user: IUserLogin): Observable<IUserLogin> {
     const apiUrl: string = 'http://localhost:8080/';
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   
-    return this.http.post<>(apiUrl, user, { headers }).pipe(
+    return this.http.post<IUserLogin>(apiUrl, user, { headers }).pipe(
       tap((response) => {
         
     })
