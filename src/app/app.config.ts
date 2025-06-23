47bec024aca8f92017e34dcf080f2797a45a07d7
Lucas <lucaspverdugo@gmail.com>
2025-06-23 18:13:22 -0300

    agrego correcciones a app.config.ts y auth.interceptor.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,46 @@
+// versión nueva
+
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+
+import { routes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { authInterceptor } from './shared/interceptors/auth.interceptor';
+import { errorInterceptor } from './shared/interceptors/error.interceptor'; // 👈 importalo
+
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { FullCalendarModule } from '@fullcalendar/angular';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideAnimations(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptor, errorInterceptor]) // 👈 ambos interceptores
+    ),
+    importProvidersFrom(FullCalendarModule),
+  ],
+};
+
+
+/*
+versión funcional de app.config.ts
+
 import {
   ApplicationConfig,
   importProvidersFrom,
@@ -29,4 +72,4 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     importProvidersFrom(FullCalendarModule),
   ],
-};
+};*/
