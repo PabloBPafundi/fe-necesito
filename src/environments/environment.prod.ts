13576d8e68b831d8afbaa2372a6387be787cf3a9
Lucas <lucaspverdugo@gmail.com>
2025-07-01 14:04:46 -0300

    envío cambios de angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://2a55-2800-810-5e7-88d-486e-3b04-6844-bec.ngrok-free.app/api'
+  apiBaseUrl: 'https://necesito.ngrok.io/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://2a55-2800-810-5e7-88d-486e-3b04-6844-bec.ngrok-free.app/api' // sin barra final
+  apiBaseUrl: 'https://necesito.ngrok.io/api' // sin barra final
 };
